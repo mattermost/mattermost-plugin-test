3e1fceb024daf151c1a63e4a1a55822abdd43c42
iomodo <wineson@gmail.com>
2020-09-03 21:56:46 +0400

    Add 5 header buttons

---
+++ webapp/src/components/icons.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const ChannelHeaderButtonIcon = () => (
+    <i
+        className='icon fa fa-plug'
+        style={{fontSize: '15px', position: 'relative', top: '-1px'}}
+    />
+);
